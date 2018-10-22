6ed5819d29c15e978a05e4c45a2557d7fcb33713
nguyenviettri <tringuyendonga@gmail.com>
2018-10-22 13:20:56 +0700

    page home

--- public/Utilities/autocomplete-suggestions/locations-autocomplete.js
+++ public/Utilities/autocomplete-suggestions/locations-autocomplete.js
@@ -68,14 +68,14 @@ $(document).ready(function(){
     ];
 
     // setup autocomplete function pulling from currencies[] array
-    $('#autocomplete').autocomplete({
-      //lookup: currencies,
-      lookup: locations,
-      onSelect: function (suggestion) {
-        //var thehtml = '<strong>Currency Name:</strong> ' + suggestion.value + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
-        $('#outputcontent').html(thehtml);
-      }
-    });
+    // $('#autocomplete').autocomplete({
+    //   //lookup: currencies,
+    //   lookup: locations,
+    //   onSelect: function (suggestion) {
+    //     //var thehtml = '<strong>Currency Name:</strong> ' + suggestion.value + ' <br> <strong>Symbol:</strong> ' + suggestion.data;
+    //     $('#outputcontent').html(thehtml);
+    //   }
+    // });
 
   });
 });
